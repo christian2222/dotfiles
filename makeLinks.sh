247898a767ed1f455f5be76fce7ea3378aeba415
xy <damsinner@gmx.de>
2020-04-05 21:28:46 +0200

    more documentation

--- makeLinks.sh
+++ makeLinks.sh
@@ -1,4 +1,5 @@
-ln -sr .vimrc ~/.vimrc
 # s symbolic link r relative to current position
+# ln -sr target(file) linkname
+ln -sr .vimrc ~/.vimrc
 ln -sr .vrapperrc ~/.vrapperrc
 ln -sr .emacs ~/.emacs
