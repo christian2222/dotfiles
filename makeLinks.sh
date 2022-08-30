65769c6794cdfbf5dcdf64fe521aa02b62e8658f
xy <damsinner@gmx.de>
2022-08-30 12:36:37 +0200

    notes for stow

--- makeLinks.sh
+++ makeLinks.sh
@@ -5,3 +5,7 @@ ln -sr .vrapperrc ~/.vrapperrc
 ln -sr .emacs ~/.emacs
 ln -sr .bash_aliases ~/.bash_aliases
 ln -sr .tmux.conf ~/.tmux.conf
+# we can also stow outside of the homedirectory
+# stow -nv dotfiles/
+# -n makes no changes to the file system
+# -v tells what would have be done
